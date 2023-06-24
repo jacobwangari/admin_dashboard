c7856a0a6ce98330f0cd690040dd6eb558456aea
jacobwangari <mwasjac@gmail.com>
2023-06-24 14:38:01 +0300

    backend and frotend merged

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, session, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 
 app = Flask(__name__)
 app.secret_key = "Secret Key"
@@ -12,30 +15,64 @@ class Users(db.Model):
     adm = db.Column(db.String(30), unique=True, nullable=False)
     name = db.Column(db.String(80), unique=False, nullable=False)
     phone = db.Column(db.String(80), unique=False, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(256), unique=False, nullable=False)
     role = db.Column(db.String(80), unique=False, nullable=False)
     
-    def __init__(self,adm,name,phone,password,role):
+    def __init__(self, adm, name, phone, password, role):
         self.adm = adm
         self.name = name
         self.phone = phone
-        self.password = password
+        self.set_password(password)
         self.role = role
         
-        
-# # Create an application context
-# with app.app_context():
-#   # Create the database tables
-#   db.create_all()  
+    @staticmethod
+    def authenticate(adm, password):
+        user = Users.query.filter_by(adm=adm).first()
+        if user and check_password_hash(user.password, password):
+            return user
+        return None
     
-
-@app.route('/')
-def home():
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
     
-    all_data = Users.query.all()
-    return render_template('index.html', users = all_data)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-@app.route('/insert', methods = ['POST'])
+
+# Routes of the app
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/academics")
+def academics():
+    return render_template('academics.html')
+
+@app.route("/news")
+def news():
+    return render_template('news.html')
+
+@app.route("/activities")
+def activities():
+    return render_template('activities.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/assignments")
+def assignments():
+    return render_template('assignments.html', assignments = Assignment)
+
+@app.route("/assignment/<string:id>")
+def assignment(id):
+    return render_template('assignment.html', id=id)
+
+@app.route('/insert', methods=['POST'])
 def insert():
     if request.method == 'POST':
         adm = request.form['adm']
@@ -44,15 +81,15 @@ def insert():
         password = request.form['password']
         role = request.form['role']
         
-        user = Users(adm,name,phone,password,role)
+        user = Users(adm, name, phone, password, role)
         db.session.add(user)
         db.session.commit()
         
-        flash("User created sucessfully")
+        flash("User created successfully")
         
-        return redirect(url_for('home'))
-    
-@app.route('/update', methods = ['GET','POST'])
+        return redirect(url_for('dashboard'))
+
+@app.route('/update', methods=['GET', 'POST'])
 def update():
     data = Users.query.get(request.form.get('id'))
     if request.method == 'POST':
@@ -64,24 +101,68 @@ def update():
         
         db.session.commit()
         
-        flash("User updated sucessfully")
+        flash("User updated successfully")
         
         return redirect(url_for('home'))
-    
-@app.route('/delete/<id>/', methods = ['GET','POST'])
+
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
 def delete(id):
     data_ = Users.query.get(id)
     db.session.delete(data_)
     db.session.commit()
         
-    flash("User deleted sucessfully")
+    flash("User deleted successfully")
         
     return redirect(url_for('home'))
-    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        adm = request.form['adm']
+        password = request.form['password']
+
+        user = Users.query.filter_by(adm=adm).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('dashboard'))
         
-    
+        flash('Invalid adm or password')
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # Clear the user ID from the session
+    session.pop('user_id', None)
+    flash('You have been logged out')
+    return redirect(url_for('login'))
 
 
-if(__name__ == "__main__"):
-    app.run(debug = True)
-    
+@app.route('/dashboard')
+def dashboard():
+    # Check if the user is logged in
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    # Get the user ID from the session
+    user_id = session['user_id']
+
+    # Get the user from the database
+    user = Users.query.get(user_id)
+
+    if user.role == 'admin':
+        # Logic for admin dashboard
+        all_data = Users.query.all()
+        return render_template('admin_dashboard.html',user= user, users=all_data)
+    elif user.role == 'teacher':
+        # Logic for teacher dashboard
+        return render_template('teacher_dashboard.html', user=user)
+    elif user.role == 'student':
+        # Logic for student dashboard
+        return render_template('student_dashboard.html', user=user)
+    else:
+        flash('Invalid user role')
+        return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
